2bcb54f57ffa8595a002f48cec970423163bdc3b
Eliza Marcum <EMarcum@eab.com>
2017-12-22 10:28:16 -0600

    Day 1, Part 2

--- lib/day1.rb
+++ lib/day1.rb
@@ -3,11 +3,18 @@ class Day1
     input = input.split(//).map(&:to_i)
     pairs = input.zip(input.rotate)
     output = pairs.inject(0) do |sum, pair|
-      if pair.first == pair.last
-        sum + pair.first
-      else
-        sum
-      end
+      addend = (pair.first == pair.last) ? pair.first : 0
+      sum + addend
+    end
+    output
+  end
+
+  def self.part2(input)
+    input = input.split(//).map(&:to_i)
+    pairs = input.zip(input.rotate(input.length/2))
+    output = pairs.inject(0) do |sum, pair|
+      addend = (pair.first == pair.last) ? pair.first : 0
+      sum + addend
     end
     output
   end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -3,7 +3,7 @@ require_relative 'spec_helper'
 # http://adventofcode.com/2017/day/1
 
 describe Day1 do
-  describe "reverse_captcha" do
+  describe "part1" do
     it "scenario 1" do
       input = "1122"
       expected = 3
@@ -26,6 +26,39 @@ describe Day1 do
       input = "91212129"
       expected = 9
       actual = Day1.part1(input)
+      actual.must_equal(expected)
+    end
+  end
+
+  describe "part2" do
+    it "scenario 1" do
+      input = "1212"
+      expected = 6
+      actual = Day1.part2(input)
+      actual.must_equal(expected)
+    end
+    it "scenario 2" do
+      input = "1221"
+      expected = 0
+      actual = Day1.part2(input)
+      actual.must_equal(expected)
+    end
+    it "scenario 3" do
+      input = "123425"
+      expected = 4
+      actual = Day1.part2(input)
+      actual.must_equal(expected)
+    end
+    it "scenario 4" do
+      input = "123123"
+      expected = 12
+      actual = Day1.part2(input)
+      actual.must_equal(expected)
+    end
+    it "scenario 5" do
+      input = "12131415"
+      expected = 4
+      actual = Day1.part2(input)
       actual.must_equal(expected)
     end
   end
