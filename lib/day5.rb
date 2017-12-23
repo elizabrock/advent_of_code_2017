f1a224d4ab47fec6f3ee25a3fdeb5beeb5bb547a
Eliza Marcum <EMarcum@eab.com>
2017-12-23 14:39:58 -0500

    Day 5, Part 2

--- lib/day5.rb
+++ lib/day5.rb
@@ -15,4 +15,25 @@ class Day5
     end
     move_count
   end
+
+  def self.part2(input)
+    items = input.split("\n").map(&:to_i)
+    current_index = 0
+    min_index = 0
+    max_index = items.length - 1
+    current_index = 0
+    move_count = 0
+    until current_index < min_index || current_index > max_index
+      move = items[current_index]
+      next_index = current_index + move
+      if move >= 3
+        items[current_index] -= 1
+      else
+        items[current_index] += 1
+      end
+      move_count += 1
+      current_index = next_index
+    end
+    move_count
+  end
 end
--- spec/day5_spec.rb
+++ spec/day5_spec.rb
@@ -11,4 +11,12 @@ describe Day5 do
       actual.must_equal(expected)
     end
   end
+  describe "part2" do
+    it "scenario 1" do
+      input = "0\n3\n0\n1\n-3"
+      expected = 10
+      actual = Day5.part2(input)
+      actual.must_equal(expected)
+    end
+  end
 end
