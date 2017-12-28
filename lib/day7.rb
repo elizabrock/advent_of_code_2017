6e544503a6ccecfe8bcf3044597d9a1dfdc887a5
Eliza Marcum <EMarcum@eab.com>
2017-12-28 17:18:10 -0500

    Buggy WIP on Day 7, Part 2
    I was too lazy to test properly :/

--- lib/day7.rb
+++ lib/day7.rb
@@ -1,36 +1,101 @@
 class Day7
   def self.part1(input)
     tree = Tree.build_tree(input)
-    tree.root
-  end
-end
-
-class Tree
-  def self.build_tree(input)
-    node_data = input.split("\n")
-    Tree.new(node_data)
+    tree.root.data
   end
 
-  attr_reader :root
-
-  def initialize(node_data)
-    @root = find_root_from(node_data)
+  def self.part2(input)
+    tree = Tree.build_tree(input)
+    unbalanced_branch = tree.root.unbalanced_branch
+    unbalanced_branch.weight - unbalanced_branch.unbalanced_by
   end
 
-  def find_root_from(node_data)
-    # discard any nodes that don't have children
-    non_leafs = node_data.find_all{ |line| line.include? "->" }
-    # discard any node that is mentioned by another node
-    loop do
-      candidate = non_leafs.pop.split(" ").first
-      unless non_leafs.detect{ |non_leaf| non_leaf.include? candidate }
-        return candidate
+  class Tree
+    def self.build_tree(input)
+      node_data = input.split("\n")
+      Tree.new(node_data)
+    end
+
+    attr_reader :root
+
+    def initialize(nodes_data)
+      root_node_data = find_root_from(nodes_data)
+      @root = Node.new(root_node_data, nodes_data)
+    end
+
+    def find_root_from(node_data)
+      # discard any nodes that don't have children
+      non_leafs = node_data.find_all{ |line| line.include? "->" }
+      # discard any node that is mentioned by another node
+      loop do
+        candidate = non_leafs.shift
+        candidate_name = candidate.split(" ").first
+        unless non_leafs.detect{ |non_leaf| non_leaf.include? candidate_name }
+          return candidate
+        end
       end
+    end
+
+    def root=(value)
+      @root = value
     end
   end
 
-  def root=(value)
-    @root = value
-  end
+  class Node
+    attr_reader :data, :weight, :children, :unbalanced_by
+    def initialize(node_data, other_nodes_data)
+      matches = node_data.match(/(\w+) \((\d+)\)[ ->]*(.*)/).to_a
+      @data = matches[1]
+      @weight = matches[2].to_i
+      child_node_names = matches[3]
+      @children = []
+      return if child_node_names.nil?
+      child_node_names.split(", ").each do |node_name|
+        child_node_data = other_nodes_data.find{ |line| line.start_with? node_name }
+        other_nodes_data.delete(child_node_data)
+        @children << Node.new(child_node_data, other_nodes_data)
+      end
+      # sort by weight, so that the oddball is either first or last
+      @children.sort!{ |a, b| a.total_weight <=> b.total_weight }
+    end
 
+    def unbalanced_branch
+      return nil if @children.empty?
+      # Ask each of my children to look for an unbalanced node
+      @children.each do |child|
+        unbalanced_descendant = child.unbalanced_branch
+        if unbalanced_descendant.nil?
+          puts "I (#{@data}) have no unbalanced deeper descendants"
+          # do nothing, keep looping
+        else
+          puts "returning my unbalanced descendant"
+          return unbalanced_descendant
+        end
+      end
+      #
+      #
+      # unbalanced_descendant = @children.map(&:unbalanced_branch).compact
+      # return unbalanced_descendant[0] unless unbalanced_descendant.empty?
+      weights = @children.map(&:total_weight)
+      unbalanced_child = nil
+      if weights.uniq.length == 1
+        return nil # The children are balanced
+      elsif weights[0] != weights[1]
+        unbalanced_child = @children[0]
+      else
+        unbalanced_child = @children.last
+      end
+      unbalanced_child.unbalanced_by = weights.last - weights.first
+      puts "Returning my (#{@data}) unbalanced child #{unbalanced_child.data}"
+      unbalanced_child
+    end
+
+    def total_weight
+      @total_weight ||= children.inject(@weight){ |sum, node| sum + node.weight }
+    end
+
+    def unbalanced_by=(value)
+      @unbalanced_by = value
+    end
+  end
 end
--- spec/day7_spec.rb
+++ spec/day7_spec.rb
@@ -3,26 +3,51 @@ require_relative 'spec_helper'
 # http://adventofcode.com/2017/day/7
 
 describe Day7 do
+  let(:input) do
+    <<~EOS
+    pbga (66)
+    xhth (57)
+    ebii (61)
+    havc (66)
+    ktlj (57)
+    fwft (72) -> ktlj, cntj, xhth
+    qoyq (66)
+    padx (45) -> pbga, havc, qoyq
+    tknk (41) -> ugml, padx, fwft
+    jptl (61)
+    ugml (68) -> gyxo, ebii, jptl
+    gyxo (61)
+    cntj (57)
+    EOS
+  end
+
   describe "part1" do
     it "scenario 1" do
-      input = <<EOS
-pbga (66)
-xhth (57)
-ebii (61)
-havc (66)
-ktlj (57)
-fwft (72) -> ktlj, cntj, xhth
-qoyq (66)
-padx (45) -> pbga, havc, qoyq
-tknk (41) -> ugml, padx, fwft
-jptl (61)
-ugml (68) -> gyxo, ebii, jptl
-gyxo (61)
-cntj (57)
-EOS
       expected = "tknk"
       actual = Day7.part1(input)
       actual.must_equal(expected)
+    end
+  end
+  describe "part2" do
+    it "scenario 1" do
+      expected = 60
+      actual = Day7.part2(input)
+      actual.must_equal(expected)
+    end
+  end
+  describe "unbalanced_branch" do
+    it "scenario 1, branch off of root" do
+      expected = "ugml"
+      root = Day7::Tree.build_tree(input).root
+      actual = root.unbalanced_branch.data
+      actual.must_equal expected
+    end
+    it "scenario 2, interior branch" do
+      expected = "ugml"
+      root = Day7::Tree.build_tree(input + "\nabcd (68) -> tknk\n").root
+      root.data.must_equal "abcd"
+      actual = root.unbalanced_branch.data
+      actual.must_equal expected
     end
   end
 end
