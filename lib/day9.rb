2870bfa367f61fa6c99e42e802fcea5fbe29b4f4
Eliza Marcum <EMarcum@eab.com>
2018-03-23 07:00:55 -0500

    Day 9, Part 2

---
+++ lib/day9.rb
@@ -0,0 +1,111 @@
+class Day9
+  def self.part1(input)
+    group = CharacterGroup.parse(input)
+    group.total_score
+  end
+
+  class CharacterGroup
+    attr_reader :groups, :parent
+
+    def self.parse(input)
+      # Remove ignored characters
+      input.gsub!(/!./,"")
+      first_char = input.slice!(0)
+      raise "bad input" unless first_char == "{"
+      group = Stream.new(input: input)
+      group
+    end
+
+    def initialize(parent: nil, input: input)
+      @parent = parent
+      @groups = []
+      unless input.nil?
+        parse(input)
+      end
+    end
+
+    def add_child(klass: CharacterGroup, input: nil)
+      self.groups << klass.new(parent: self, input: input)
+    end
+
+    def size
+      groups.inject(1){|sum, group| sum + group.size }
+    end
+
+    def score
+      return @score if @score
+      if parent.nil?
+        @score = 1
+      else
+        @score = parent.score + 1
+      end
+    end
+
+    def total_score
+      self.groups.inject(self.score){|sum, group| sum + group.total_score }
+    end
+
+    private
+
+    def parse(input)
+      until input.empty? do
+        # Scan until you see an "opening" character of "{" or "<"
+        # Start a new group (Stream, or Garbage)
+        # Delegate the remainder of the scan to the new group
+        # Rinse repeat with what remains
+        next_char = input.slice!(0)
+        if ends_group?(next_char)
+          return
+        end
+        next_class = group_started_by(next_char)
+        unless next_class.nil?
+          self.add_child(klass: next_class, input: input)
+        end
+      end
+    end
+
+    def ends_group?(char)
+      false
+    end
+
+    def group_started_by(char)
+      case char
+      when "{"
+        Stream
+      when "<"
+        Garbage
+      else
+        nil
+      end
+    end
+  end
+
+  class Stream < CharacterGroup
+
+    private
+
+    def ends_group?(char)
+      char == "}"
+    end
+  end
+
+  class Garbage < CharacterGroup
+    def score
+      0
+    end
+
+    def size
+      0
+    end
+
+    private
+
+    def ends_group?(char)
+      char == ">"
+    end
+
+    def group_started_by(char)
+      nil
+    end
+  end
+end
---
+++ spec/day9_spec.rb
@@ -0,0 +1,165 @@
+require_relative 'spec_helper'
+
+# http://adventofcode.com/2017/day/9
+
+describe Day9 do
+  describe "part1" do
+    it "scenario 1" do
+      expected = 1
+      actual = Day9.part1("{}")
+      actual.must_equal(expected)
+    end
+    it "scenario 2" do
+      expected = 6
+      actual = Day9.part1("{{{}}}")
+      actual.must_equal(expected)
+    end
+    it "scenario 3" do
+      expected = 5
+      actual = Day9.part1("{{},{}}")
+      actual.must_equal(expected)
+    end
+    it "scenario 4" do
+      expected = 16
+      actual = Day9.part1("{{{},{},{{}}}}")
+      actual.must_equal(expected)
+    end
+    it "scenario 5" do
+      expected = 1
+      actual = Day9.part1("{<a>,<a>,<a>,<a>}")
+      actual.must_equal(expected)
+    end
+    it "scenario 6" do
+      expected = 9
+      actual = Day9.part1("{{<ab>},{<ab>},{<ab>},{<ab>}}")
+      actual.must_equal(expected)
+    end
+    it "scenario 7" do
+      expected = 9
+      actual = Day9.part1("{{<!!>},{<!!>},{<!!>},{<!!>}}")
+      actual.must_equal(expected)
+    end
+    it "scenario 8" do
+      expected = 3
+      actual = Day9.part1("{{<a!>},{<a!>},{<a!>},{<ab>}}")
+      actual.must_equal(expected)
+    end
+  end
+end
+
+describe Day9::CharacterGroup do
+  describe "parse" do
+    it "scenario 1, one group" do
+      expected = 1
+      group = Day9::CharacterGroup.parse("{}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 2, two groups" do
+      expected = 2
+      group = Day9::CharacterGroup.parse("{{}}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 3, short garbage" do
+      expected = 1
+      group = Day9::CharacterGroup.parse("{<>}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 4, long garbage" do
+      expected = 1
+      group = Day9::CharacterGroup.parse("{<{},{},{{}}>}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 5" do
+      expected = 3
+      group = Day9::CharacterGroup.parse("{{{}}}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 6" do
+      expected = 3
+      group = Day9::CharacterGroup.parse("{{},{}}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 7" do
+      expected = 6
+      group = Day9::CharacterGroup.parse("{{{},{},{{}}}}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 8" do
+      expected = 1
+      group = Day9::CharacterGroup.parse("{<a>,<a>,<a>,<a>}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 9" do
+      expected = 5
+      group = Day9::CharacterGroup.parse("{{<a>},{<a>},{<a>},{<a>}}")
+      group.size.must_equal(expected)
+    end
+    it "scenario 10" do
+      expected = 2
+      group = Day9::CharacterGroup.parse("{{<!>},{<!>},{<!>},{<a>}}")
+      group.size.must_equal(expected)
+    end
+  end
+
+  describe "score" do
+    it "is one more than the parent object" do
+      parent = MiniTest::Mock.new
+      parent.expect :score, 12
+      parent.expect :nil?, false
+      character_group = Day9::CharacterGroup.new(parent: parent)
+      character_group.score.must_equal 13
+    end
+
+    it "is 1 if there is no parent object" do
+      character_group = Day9::CharacterGroup.new()
+      character_group.score.must_equal 1
+    end
+  end
+
+  describe "size" do
+    it "is 1, if there are no children" do
+      character_group = Day9::CharacterGroup.new()
+      character_group.size.must_equal 1
+    end
+
+    it "is the number of descendants if there are children" do
+      character_group = Day9::CharacterGroup.new()
+      character_group.add_child
+      character_group.add_child
+      child = character_group.groups.last
+      child.add_child
+      child.add_child
+      character_group.size.must_equal 5
+    end
+
+    it "ignores garbage" do
+      character_group = Day9::CharacterGroup.new()
+      character_group.add_child
+      character_group.add_child(klass: Day9::Garbage)
+      character_group.add_child
+      child = character_group.groups.last
+      child.add_child
+      child.add_child
+      character_group.size.must_equal 5
+    end
+  end
+
+  describe "total_score" do
+    it "is the same as score, if there are no children" do
+      character_group = Day9::CharacterGroup.new()
+      character_group.stub(:score, 2) do
+        character_group.total_score.must_equal 2
+      end
+    end
+
+    it "is the object's score plus the children's score if there are children" do
+      character_group = Day9::CharacterGroup.new()
+      character_group.add_child
+      character_group.add_child(klass: Day9::Garbage)
+      character_group.add_child
+      child = character_group.groups.first
+      child.add_child
+      character_group.total_score.must_equal 8 # 1 + 2 + 2 + 3
+    end
+  end
+end
